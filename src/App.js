3452f9d4b32920ff842eaf9d427dd8a7807b256d
CSapien08 <sapiano08@gmail.com>
2021-07-27 15:23:49 -0700

    added winning possibilities

--- src/App.js
+++ src/App.js
@@ -21,13 +21,44 @@ class App extends Component{
       if(turn % 2 !== 0){
         squares[index] = 'X'
         clicked[index] = 1
+        this.calculateWinner()
+        
         this.setState({squares: squares, turn: turn + 1, clicked: clicked})
       }
       else{
         squares[index] = 'O'
         clicked[index] = 2
+        
+        this.calculateWinner()
         this.setState({squares: squares, turn: turn + 1, clicked: clicked})
       }
+  }
+  calculateWinner = () => {
+    const {clicked} = this.state
+
+    const possibleLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 8]
+    ]
+    
+    for(let i=0; i < possibleLines.length; i++){
+      let winningRow = possibleLines[i]
+      let p1 = winningRow[0]
+      let p2 = winningRow[1]
+      let p3 = winningRow[2]
+      
+      if(clicked[p1] === clicked[p2] && clicked[p1] === clicked[p3] && clicked[p2] === clicked[p3] && clicked[p1] !== 0 && clicked[p2] !== 0 && clicked[p3] !==0 ){
+          alert("winner")
+          this.setState({win: true})
+      }
+
+    }
   }
 
   render(){
